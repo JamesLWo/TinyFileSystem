3d16a2d730f8995bf72091d8994c6429719c360b
James Wo <jameswo1234@gmail.com>
2021-04-28 22:38:06 -0400

    Update tfs.c

--- tfs.c
+++ tfs.c
@@ -550,12 +550,12 @@ int tfs_mkfs() {
 	struct inode root_inode;
 	root_inode.ino = 0; //0 as 'well-known' ino
 	root_inode.type = 0; //0 for directory, 0 for file
-	printf("Verifying root_inode.ino: %d should be 0, root_inode.type: %d should be 0\n");
+	printf("Verifying root_inode.ino: %d should be 0, root_inode.type: %d should be 0\n", root_inode.ino, root_inode.type);
 
 	//write to disk
 
 	printf("writing root_inode to block 1...\n");
-	bio_write(1, &root_inode);
+	writei(root_inode.ino, &root_inode);
 	printf("write successful\n");
 	
 	printf("---------------------------------------\n");
