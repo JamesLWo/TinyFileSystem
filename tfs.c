576bfce420daee0717c70afae0e72d0e989fe559
James Wo <jameswo1234@gmail.com>
2021-05-03 03:45:49 -0400

    Update tfs.c

--- tfs.c
+++ tfs.c
@@ -1338,6 +1338,7 @@ static int tfs_rmdir(const char *path) {
 		if(data_block_to_clear == -1){
 			break;
 		}
+		printf("unsetting this bit: %d\n", get_bitmap(data_region_bitmap, data_block_to_clear));
 		unset_bitmap(data_region_bitmap, data_block_to_clear);
 	}
 	bio_write(2, data_region_bitmap);
@@ -1423,6 +1424,7 @@ static int tfs_unlink(const char *path) {
 		if(current_data_block_number == -1){
 			break;
 		}
+		printf("unsetting this bit: %d\n", get_bitmap(data_region_bitmap, current_data_block_number));
 		unset_bitmap(data_region_bitmap, current_data_block_number);
 	}
 	bio_write(2, data_region_bitmap);
